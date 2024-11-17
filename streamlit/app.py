06ea7f8469f2c1e90453038ecd39f746eb0d4036
Ganesh Ram B <76607195+ganeshramb@users.noreply.github.com>
2024-11-17 21:07:04 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 
 
 # Load model
-model = tf.keras.models.load_model("final_stock_prediction")
+model = tf.keras.models.load_model("stock_prediction.keras")
 
 # Web App Header and Sidebar
 st.title('Advanced Stock Price Prediction App 📈')
